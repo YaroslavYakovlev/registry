e571be9140dafadfc119b65683f852fcb63c247e
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-04-13 21:02:38 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,25 @@
-#include <algorithm>
 #include <iostream>
 #include <map>
 #include <string>
 
-void sort(std::map<int, std::string> turn) {
-  std::cout << "[sort]" << std::endl;
-}
-
 int main() {
   std::cout << "Registry" << std::endl;
-  std::map<int, std::string> turn;
+  std::map<std::string, int> turn;
   const std::string next = "Next";
   std::string command;
   int count = 0;
   do {
     std::cout << "Enter command (Surname/Next)" << std::endl;
     std::cin >> command;
-    if (command == next) {
-      turn.erase(count);
-      count--;
-    } else if (!command.empty() && command != next) {
+    if (!command.empty() && command != next) {
       count++;
-      turn.insert(std::make_pair(count, command));
-      sort(turn);
+      turn.insert(std::make_pair(command, count));
+    } else if (command == next) {
+      turn.erase(command);
+      count--;
     }
-    for (std::map<int, std::string>::iterator it = turn.begin(); it != turn.end(); it++) {
-      std::cout << "Number: " << it->first << "; Surname: " << it->second << std::endl;
+    for (std::map<std::string, int>::iterator it = turn.begin(); it != turn.end(); it++) {
+      std::cout << "Surname: " << it->first << "; Number: " << it->second << std::endl;
     }
   } while (count > 0);
 
