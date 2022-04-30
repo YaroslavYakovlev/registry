dab72ad9926859374afe9058472341f612c1af07
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-04-30 16:26:31 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,22 @@ int main() {
   do {
     std::cout << "Enter command (Surname/Next)" << std::endl;
     std::cin >> command;
-    if (turn.empty() && command == next) {
-      std::cout << "The queue is empty" << std::endl;
-      restart = true;
-    } else if (!command.empty() && command != next) {
+    if (command == next) {
+      if (turn.empty()) {
+        std::cout << "The queue is empty" << std::endl;
+        restart = true;
+      } else {
+        std::cout << "Gone Surname: " << turn.begin()->first << std::endl;
+        turn.erase(turn.begin()->first);
+        count--;
+      }
+    } else if (!command.empty()) {
       count++;
       turn.insert(std::make_pair(command, count));
-    } else if (command == next) {
-      std::cout << "Gone Surname: " << turn.begin()->first << std::endl;
-      turn.erase(turn.begin()->first);
-      count--;
     }
     for (std::map<std::string, int>::iterator it = turn.begin(); it != turn.end(); it++) {
       std::cout << "Surname: " << it->first << "; Number: " << it->second << std::endl;
     }
-    if (count <= 0)
-      restart = false;
   } while (count > 0 || restart);
 
   return 0;
